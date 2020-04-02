021a383284e1dc9964b362e43d0b537344123d83
manuel <manuel@dinamiq.com>
2020-04-02 10:57:11 +0000

    Quitar texto mensaje olvidar contraseña

--- exiled/config/backpack/base.php
+++ exiled/config/backpack/base.php
@@ -71,7 +71,7 @@ return [
     // ------
 
     // Menu logo. You can replace this with an <img> tag if you have a logo.
-    'project_logo'   => '<img src="/assets/images/logo.png" style="max-width: 120px;">',
+    'project_logo'   => '<img src="/assets/images/logo_mini.png">',
 
     // Show / hide breadcrumbs on admin panel pages.
     'breadcrumbs' => true,
