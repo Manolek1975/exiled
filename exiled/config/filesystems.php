76e3c2d5ecd24f37fcc883dee92b9a8c5c7f1bdb
manuel <manuel@dinamiq.com>
2020-04-02 10:40:40 +0000

    Config

--- exiled/config/filesystems.php
+++ exiled/config/filesystems.php
@@ -55,14 +55,19 @@ return [
             'visibility' => 'public',
         ],
 
-        's3' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'endpoint' => env('AWS_URL'),
+        'uploads' => [ 
+            'driver' => 'local', 
+            'root' => public_path('uploads'),
         ],
+        
+        // 's3' => [
+        //     'driver' => 's3',
+        //     'key' => env('AWS_ACCESS_KEY_ID'),
+        //     'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        //     'region' => env('AWS_DEFAULT_REGION'),
+        //     'bucket' => env('AWS_BUCKET'),
+        //     'endpoint' => env('AWS_URL'),
+        // ],
 
     ],
 
--- exiled/routes/backpack/custom.php
+++ exiled/routes/backpack/custom.php
@@ -11,4 +11,23 @@ Route::group([
     'middleware' => ['web', config('backpack.base.middleware_key', 'admin')],
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
+
+    function redirUrl() {
+        //return config('backpack.base.route_prefix').'/dashboard'; <- Antes
+        return redirect('http://exiled.local/'.config('backpack.base.route_prefix').'/dashboard');
+    }
+    // Deshabilitamos las rutas para la informacion de la cuenta
+    Route::get('edit-account-info',function () { return redirUrl();  } )->name ('backpack.account.info');
+    Route::post('edit-account-info', function () { return redirUrl();  } );
+    Route::get('change-password', function () { return redirUrl();  } )->name('backpack.account.password');
+    Route::post('change-password', function () { return redirUrl();  } );
+    // Deshabilitamos las rutas para el reseteo de password
+    Route::get('password/reset', function () { return redirUrl();  })->name('backpack.auth.password.reset');
+    Route::post('password/reset', function () { return redirUrl();  });
+    Route::get('password/reset/{token}', function () { return redirUrl();  })->name('backpack.auth.password.reset.token');
+    Route::post('password/email', function () { return redirUrl();  })->name('backpack.auth.password.email');
+    // Deshabilitamos las rutas para el registro (teoricamente esto ya se hace en config/backpack/base.php pero por si acaso )
+    Route::get('register', function () { return redirUrl();  } )->name('backpack.auth.register');
+    Route::post('register', function () { return redirUrl();  } );
+
 }); // this should be the absolute last line of this file
